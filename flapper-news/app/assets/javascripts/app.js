e87d508f9e31c3b4888a7ea3206176b8738bc1cb
Andrew Menning <menning23@gmail.com>
2016-03-15 21:51:07 -0400

    added angular-rails-templates and separated templates to separate files

--- flapper-news/app/assets/javascripts/app.js
+++ flapper-news/app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-angular.module('flapperNews', ['ui.router'])
+angular.module('flapperNews', ['ui.router', 'templates'])
 
 .config([
 '$stateProvider',
@@ -8,13 +8,13 @@ function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: '/home',
-      templateUrl: '/home.html',
+      templateUrl: 'home/_home.html',
       controller: 'MainCtrl'
     })
     
     .state('posts', {
   		url: '/posts/{id}',
-  		templateUrl: '/posts.html',
+  		templateUrl: 'posts/_posts.html',
   		controller: 'PostsCtrl'
 	});
 
